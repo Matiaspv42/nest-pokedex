711ee7ab4a215cd2152fd83bca7d58f640faacef
Matias <mpintov42@gmail.com>
2023-07-20 20:39:32 -0400

    antesss

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -8,6 +8,11 @@ export class SeedService {
  
   async executeSeed(){
     const {data} = await this.axios.get<PokeRespone>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    data.results.forEach(({name, url})=> {
+      const segmets = url.split('/')
+      const no: number = +segmets[segmets.length - 2]
+      
+    })
     return data
   }
 }
